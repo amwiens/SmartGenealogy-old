33c53799c517ab4ea1a7b2fc50e5a9f7969f5cf9
Aaron Wiens <aaron@awwebsolutions.com>
2023-06-15 16:04:19 -0500

    Created project.

---
+++ src/SmartGenealogy/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SmartGenealogy
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
