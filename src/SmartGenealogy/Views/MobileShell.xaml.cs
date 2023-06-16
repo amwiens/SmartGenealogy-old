2bb1ab61852d3a95f4abfc84dedfedfcbf82b57c
Aaron Wiens <aaron@awwebsolutions.com>
2023-06-16 09:07:04 -0500

    CodeMaid: cleaned up code.

--- src/SmartGenealogy/Views/MobileShell.xaml.cs
+++ src/SmartGenealogy/Views/MobileShell.xaml.cs
@@ -2,8 +2,8 @@ namespace SmartGenealogy.Views;
 
 public partial class MobileShell : Shell
 {
-	public MobileShell()
-	{
-		InitializeComponent();
-	}
+    public MobileShell()
+    {
+        InitializeComponent();
+    }
 }
