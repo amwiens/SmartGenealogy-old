31b98bba604b16966c41d2c6959456a935b1b936
Aaron Wiens <aaron@awwebsolutions.com>
2023-06-16 11:44:01 -0500

    Added navigation and styling.

---
+++ src/SmartGenealogy.Maui/MVVM/BaseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SmartGenealogy.Maui.MVVM;
+
+public class BaseViewModel : ObservableRecipient
+{
+
+    protected BaseViewModel()
+    {
+
+    }
+
+    //[RelayCommand]
+    //protected virtual Task BackAsync() => AppNavigator.GoBackAsync();
+}
