4033749221006ec027e042378ed13ebcea87d412
Aaron Wiens <aaron@awwebsolutions.com>
2023-07-12 20:20:54 -0500

    #7 Created the buttons and the commands for the file page. (#8)

--- src/SmartGenealogy/ViewModels/File/FilePageViewModel.cs
+++ src/SmartGenealogy/ViewModels/File/FilePageViewModel.cs
@@ -1,9 +1,8 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 
 using SmartGenealogy.Messages;
-
-using System.Diagnostics;
 
 namespace SmartGenealogy.ViewModels;
 
@@ -11,19 +10,64 @@ public partial class FilePageViewModel : MainPageViewModelBase
 {
     private AppSettings.AppSettings _appSettings => AppSettings.AppSettings.Instance;
 
+    [ObservableProperty]
+    private bool _isFileOpen;
+
+    [ObservableProperty]
+    private string? _currentFile;
+
+    [RelayCommand]
+    private void CreateFile()
+    {
+        _appSettings.FilePath = "create";
+        IsFileOpen = true;
+        CurrentFile = $"CurrentFile: {_appSettings.FilePath}";
+        WeakReferenceMessenger.Default.Send(new OpenFileChangedMessage(true));
+    }
+
     [RelayCommand]
     private void OpenFile()
     {
-        _appSettings.FilePath = "test";
+        _appSettings.FilePath = "open";
+        IsFileOpen = true;
+        CurrentFile = $"CurrentFile: {_appSettings.FilePath}";
         WeakReferenceMessenger.Default.Send(new OpenFileChangedMessage(true));
-        Debug.WriteLine("File Open");
+    }
+
+    [RelayCommand]
+    private void RestoreFile()
+    {
+        _appSettings.FilePath = "restore";
+        IsFileOpen = true;
+        CurrentFile = $"CurrentFile: {_appSettings.FilePath}";
+        WeakReferenceMessenger.Default.Send(new OpenFileChangedMessage(true));
+    }
+
+    [RelayCommand]
+    private void BackupFile()
+    {
+    }
+
+    [RelayCommand]
+    private void ImportData()
+    {
+    }
+
+    [RelayCommand]
+    private void ExportData()
+    {
+    }
+
+    [RelayCommand]
+    private void Tools()
+    {
     }
 
     [RelayCommand]
     private void CloseFile()
     {
         _appSettings.FilePath = null;
+        IsFileOpen = false;
         WeakReferenceMessenger.Default.Send(new OpenFileChangedMessage(false));
-        Debug.WriteLine("File Closed");
     }
 }
